d6aae65789e53a524ad14c292b3593e95e37fb95
senadmediu <sm28748@seeu.edu.mk>
2023-06-25 14:35:16 +0200

    Initial Commit

--- src/common/Cart/Cart.jsx
+++ src/common/Cart/Cart.jsx
@@ -13,7 +13,7 @@ const Cart = ({ CartItem, addToCart, decreaseQty }) => {
           {/* if hamro cart ma kunai pani item xaina bhane no diplay */}
 
           <div className='cart-details'>
-            {CartItem.length === 0 && <h1 className='no-items product'>No Items are add in Cart</h1>}
+            {CartItem.length === 0 && <h1 className='no-items product'>No Items are added in Cart</h1>}
 
             {/* yasma hami le cart item lai display garaaxa */}
             {CartItem.map((item) => {
